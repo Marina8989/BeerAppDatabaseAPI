8e6c6b0f06c8b41958e2616a8bf3bf197dbc44c3
Marina8989 <marina.mill209@gmail.com>
2021-03-30 14:33:31 -0700

    added more css for better UX/UI

--- main.js
+++ main.js
@@ -3,12 +3,26 @@ const urlBase = 'https://api.punkapi.com/v2/beers';
 async function getBeers() {
     const result = await fetch(urlBase);
     const data = await result.json();
+    console.log(data);
+    const beersDiv = document.querySelector('.beers');
 
     let html = '';
 
     data.forEach(item => {
-        html+=`<p>${item.name}</p>`;
+
+        html+=`
+        <div class='beer-wrapper card'>
+          <div class='beer'>
+            <img class='beer__img' src='${item.image_url}'>
+            <h3>${item.name}</h3>
+            <span class='beer__info'>
+               <span>ABV: ${item.abv}%</span>
+               <span>IBU: ${item.ibu}</span>
+            </span>
+          </div>
+        </div>    
+        `;
     })
-    document.querySelector('.beers').innerHTML = html;
+    beersDiv.innerHTML = html;
 }
 getBeers();
