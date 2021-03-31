46934fe8ec497fadae721ec8892761d7b8da9c6f
Marina8989 <marina.mill209@gmail.com>
2021-03-30 17:47:31 -0700

    added all js, css and html to project

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const urlBase = 'https://api.punkapi.com/v2/beers';
+const urlBase = 'https://api.punkapi.com/v2/beers?page=';
 const filterABV = document.getElementById('filterABV');
 const filterIBU = document.getElementById('filterIBU');
+const pageText = document.getElementById('pageNumber');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
 let optionsABV = '';
 let optionsIBU = '';
+let page = 1;
 
 //filter
 filterABV.addEventListener('change', e => {
@@ -13,14 +17,15 @@ filterABV.addEventListener('change', e => {
              optionsABV = '';
              break;
           case 'weak':
-              optionsABV = 'abv_lt=4.6';
+              optionsABV = '&abv_lt=4.6';
               break;
           case 'medium':
-              optionsABV = 'abv_gt=4.5&abv_lt=7.6';
+              optionsABV = '&abv_gt=4.5&abv_lt=7.6';
               break;
           case 'strong':
-              optionsABV = 'abv_gt=7.5';           
+              optionsABV = '&abv_gt=7.5';           
     }
+    page = 1;
     getBeers();
 })
 
@@ -34,36 +39,50 @@ filterIBU.addEventListener('change', e => {
              optionsIBU = '';
              break;
          case 'weak':
-             optionsIBU = 'ibu_lt=35';
+             optionsIBU = '&ibu_lt=35';
              break;
          case 'medium':
-             optionsIBU = 'ibu_gt=34&ibu_lt=75';
+             optionsIBU = '&ibu_gt=34&ibu_lt=75';
              break;
          case 'strong':
-             optionsIBU = 'ibu_gt=74';
+             optionsIBU = '&bu_gt=74';
              break;            
     }
+    page = 1;
     getBeers();
 })
 
 
 async function getBeers() {
-    const url = urlBase + '?' + optionsABV + '&' +optionsIBU;
-    console.log(url)
-
+    const url = urlBase + page + optionsABV + optionsIBU;
+    console.log(url);
     const result = await fetch(url);
     const data = await result.json();
     console.log(data);
+    pageText.innerText = page;
+
+    if(page === 1) {
+        prevPage.disabled = true;
+    } else {
+        prevPage.disabled = false;
+    } 
+    if(data.length < 25) {
+       nextPage.disabled = true;
+    }else {
+        nextPage.disabled = false;
+    }
+
     const beersDiv = document.querySelector('.beers');
 
     let html = '';
+    const genericBottle = 'https://cdn.pixabay.com/photo/2014/12/22/00/04/bottle-576717_960_720.png';
 
     data.forEach(item => {
 
         html+=`
         <div class='beer-wrapper card'>
           <div class='beer'>
-            <img class='beer__img' src='${item.image_url}'>
+            <img class='beer__img' src='${item.image_url ? item.image_url : genericBottle}'>
             <h3>${item.name}</h3>
             <span class='beer__info'>
                <span>ABV: ${item.abv}%</span>
@@ -83,4 +102,15 @@ async function getBeers() {
     })
     beersDiv.innerHTML = html;
 }
+
+//pagination
+prevPage.addEventListener('click', () => {
+    page--;
+    getBeers();
+});
+nextPage.addEventListener('click', () => {
+    page++;
+    getBeers();
+})
+
 getBeers();
